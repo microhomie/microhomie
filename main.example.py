0b645ed8dc9ee31a701f500a00b2412c93b14484
Dennis Schwertel <dennisschwertel@gmail.com>
2018-02-06 22:30:50 +0100

    removed duplicated if

--- main.example.py
+++ main.example.py
@@ -20,8 +20,7 @@ homie_device.publish_properties()
 
 while True:
     # try wifi reconnect in case it loses connection
-    if not utils.wlan.isconnected():
-        utils.wifi_connect()
+    utils.wifi_connect()
 
     # Update the data of the simple note for demonstration purpose
     n.value = utime.time()
