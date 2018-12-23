314c2ba6585c5ff633ede89006268e3e8294a6bd
Rafael Römhild <rafael@roemhild.de>
2018-12-24 00:14:43 +0100

    automate properties

--- homie/node/__init__.py
+++ homie/node/__init__.py
@@ -1,35 +1,85 @@
 from utime import time
 
 
-class HomieNode(object):
-    def __init__(self, name, interval=60):
+class HomieNodeProperty:
+    def __init__(
+        self,
+        id,
+        name=None,
+        settable=False,
+        retained=True,
+        unit=None,
+        datatype="string",
+        format=None,
+    ):
+        self.id = id
         self.name = name
-        self._node_id = None
-        self.interval = interval
-        self.next_update = time()
+        self.settable = settable
+        self.retained = retained
+        self.unit = unit
+        self.datatype = datatype
+        self.format = format
+        self._data = None
 
     def __repr__(self):
-        return "{}(name={!r}, pin={!r}, interval={!r})".format(
-            self.__class__.__name__, self.name, self.pin, self.interval
+        return "{}(id={!r}, name={!r}, settable={!r}, retained={!r}, unit={!r}, datatype={!r}, format={!r})".format(
+            self.__class__.__name__,
+            self.id,
+            self.name,
+            self.settable,
+            self.retained,
+            self.unit,
+            self.datatype,
+            self.format,
+        )
+
+        def __str__(self):
+            return self.id
+
+        @property
+        def data(self):
+            return self._data
+
+        @data.setter
+        def data(self, val):
+            if isinstance(val, bool):
+                val = str(val).lower()
+            self._data = val
+
+
+class HomieNode:
+    def __init__(self, id, name, type, interval=60):
+        self.id = id
+        self.name = name
+        self.type = type
+        self._interval = interval
+        self._next_update = time()
+        self._properties = []
+        self._subscribe = []
+
+    def __repr__(self):
+        return "{}(id={!r}, name={!r}, type={!r}, interval={!r})".format(
+            self.__class__.__name__,
+            self.id,
+            self.name,
+            self.type,
+            self._interval,
         )
 
     def __str__(self):
         """Return nice information about the object"""
         raise NotImplementedError("not implemented")
 
-    @property
+    def add_property(self, p):
+        self._properties.append(p)
+        if p.settable:
+            self._subscribe.append(
+                b"{}/{}/set".format(self.id, p.id,)
+            )
+
     def subscribe(self):
-        return ()
-
-    @property
-    def node_id(self):
-        if self._node_id is None:
-            raise NotImplementedError("missing node id")
-        return self._node_id
-
-    @node_id.setter
-    def node_id(self, val):
-        self._node_id = val
+        for t in self._subscribe:
+            yield t
 
     def has_update(self):
         """Depending on the interval:
@@ -38,19 +88,47 @@ class HomieNode(object):
         returns False if its not yet time for an update
         """
         _time = time
-        if _time() > self.next_update:
+        if _time() > self._next_update:
             self.update_data()
-            self.next_update = _time() + self.interval
+            self._next_update = _time() + self._interval
             return True
         return False
 
     def get_properties(self):
         """General properties of this node"""
-        raise NotImplementedError("not implemented")
+        nid = self.id
+        yield (b"{}/$name".format(nid), self.name)
+        yield (b"{}/$type".format(nid), self.type)
+
+        if self._properties:
+            yield (b"{}/$properties".format(nid), b",".join(self._properties))
+
+            for p in self._properties:
+                t = "{}/{}".format(nid, p.id)
+                if p.name:
+                    yield (b"{}/$name".format(t), p.name)
+
+                if p.settable:
+                    yield (b"{}/$settable".format(t), b"true")
+
+                if p.retained is False:
+                    yield (b"{}/$retained".format(t), b"false")
+
+                if p.unit:
+                    yield (b"{}/$unit".format(t), p.unit)
+
+                if p.datatype != "string":
+                    yield (b"{}/$datatype".format(t), p.datatype)
+
+                if p.datatype in ["enum", "color"]:
+                    yield (b"{}/$format".format(t), p.format)
 
     def get_data(self):
         """Return the current values"""
-        raise NotImplementedError("not implemented")
+        nid = self.id
+        for p in self._properties:
+            if p.data is not None:
+                yield (b"{}/{}".format(nid, p.id), p.data, p.retained)
 
     def update_data(self):
         """Prepare new data. Measure nodes... """
