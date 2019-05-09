9e0ee6bfddd24dd0d968521700f5e99abe6d62dc
Rafael Römhild <rafael@roemhild.de>
2019-05-09 16:32:34 +0200

    Add One-Wire example node for ds18b20 (Thanks Rick @palmtreefrb)

---
+++ examples/ds18b20/README.rst
@@ -0,0 +1,7 @@
+=====================================
+Microhomie ds18b20 temperature sensor
+=====================================
+
+This is an example node for the One-Wire ds18b20 temperature sensor, shared by `palmtreefrb <https://github.com/microhomie/microhomie/issues/31>`_
+
+This example will only return the first sensor on the bus as One-Wire can have many sensors on one gpio. It can be quite easily be modified to enumerate all sensors.
---
+++ examples/ds18b20/main.py
@@ -0,0 +1,78 @@
+import time
+
+from ds18x20 import DS18X20
+from onewire import OneWire
+
+from machine import Pin
+from uasyncio import get_event_loop, sleep_ms
+
+from homie.node import HomieNode
+from homie.property import HomieNodeProperty
+
+
+class DS18B20(HomieNode):
+
+    def __init__(self, name="One Wire DS18B20", pin=12, interval=10, pull=-1):
+        super().__init__(id="ds18b20", name=name, type="ds18b20")
+        self.ds18b20 = DS18X20(OneWire(Pin(pin)))
+        addrs = self.ds18b20.scan()
+        if not addrs:
+            raise Exception('no DS18B20 found at bus on pin %d' % pin)
+        # save what should be the only address found
+        self.addr = addrs.pop()
+
+        self.temperature = 0
+
+        self.interval = interval
+
+        self.temp_property = HomieNodeProperty(
+            id="temperature",
+            name="Temperature",
+            datatype="float",
+            format="-40:80",
+            unit="°F",
+        )
+        self.add_property(self.temp_property)
+
+        loop = get_event_loop()
+        loop.create_task(self.update_data())
+
+    async def update_data(self):
+        delay = self.interval * 1000
+
+        while True:
+            self.temperature = self.read_temp()
+            self.temp_property.data = self.temperature
+            await sleep_ms(delay)
+
+    def read_temp(self, fahrenheit=True):
+        """
+        Reads temperature from a single DS18X20
+        :param fahrenheit: Whether or not to return value in Fahrenheit
+        :type fahrenheit: bool
+        :return: Temperature
+        :rtype: float
+        """
+        self.ds18b20.convert_temp()
+        time.sleep_ms(750)
+        temp = self.ds18b20.read_temp(self.addr)
+        if fahrenheit:
+            ntemp = temp
+            print('Temp: ' + str(self.c_to_f(ntemp)))
+            return self.c_to_f(ntemp)
+        return temp
+
+    @staticmethod
+    def c_to_f(c):
+        """
+        Converts Celsius to Fahrenheit
+        :param c: Temperature in Celsius
+        :type c: float
+        :return: Temperature in Fahrenheit
+        :rtype: float
+        """
+        return (c * 1.8) + 32
+
+
+if __name__ == "__main__":
+    main()
