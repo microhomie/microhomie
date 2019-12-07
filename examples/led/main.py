723f5f4b0e790fbbf280d32b3455d3237dffd31d
Rafael Römhild <rafael@roemhild.de>
2019-12-07 16:49:25 +0100

    LED example: Add device button to toggle led

--- examples/led/main.py
+++ examples/led/main.py
@@ -1,9 +1,12 @@
 import settings
+
+from machine import Pin
+from aswitch import Pushbutton
+
 from homie.constants import FALSE, TRUE, BOOLEAN
 from homie.device import HomieDevice
 from homie.node import HomieNode
 from homie.property import HomieNodeProperty
-from machine import Pin
 
 
 # reversed values for the esp8266 boards onboard led
@@ -15,6 +18,8 @@ class LED(HomieNode):
         super().__init__(id="led", name=name, type="LED")
         self.pin = pin
         self.led = Pin(pin, Pin.OUT, value=0)
+        self.btn = Pushbutton(Pin(0, Pin.IN, Pin.PULL_UP))
+        self.btn.press_func(self.toggle_led)
 
         self.power_property = HomieNodeProperty(
             id="power",
@@ -29,6 +34,14 @@ class LED(HomieNode):
     def on_power_msg(self, topic, payload, retained):
         self.led(ONOFF[payload])
         self.power_property.data = ONOFF[self.led()]
+
+    def toggle_led(self):
+        if self.power_property.data != TRUE:
+            self.led(0)
+            self.power_property.data = TRUE
+        else:
+            self.led(1)
+            self.power_property.data = FALSE
 
 
 def main():
