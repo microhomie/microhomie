9b6e86848bd4e77384ad565315461df290a985d0
Rafael Römhild <rafael@roemhild.de>
2019-12-26 01:07:38 +0100

    Add inisetup.py

--- manifest.py
+++ manifest.py
@@ -2,28 +2,41 @@
 freeze("$(MPY_DIR)/extmod/webrepl", ("webrepl.py", "websocket_helper.py",))
 
 # Modules
-freeze('$(PORT_DIR)/modules')
+freeze(
+    "$(PORT_DIR)/modules",
+    (
+        "_boot.py",
+        "apa102.py",
+        "flashbdev.py",
+        "neopixel.py",
+        "ntptime.py",
+        "port_diag.py",
+    ),
+)
 
 # Tools
-freeze('$(MPY_DIR)/tools', ('upip.py', 'upip_utarfile.py'))
+freeze("$(MPY_DIR)/tools", ("upip.py", "upip_utarfile.py"))
 
 # drivers
-freeze('$(MPY_DIR)/drivers/dht', 'dht.py')
-freeze('$(MPY_DIR)/drivers/onewire')
+freeze("$(MPY_DIR)/drivers/dht", "dht.py")
+freeze("$(MPY_DIR)/drivers/onewire")
 
 # Microhomie
-include
-freeze(".", "homie/__init__.py")
-freeze(".", "homie/constants.py")
-freeze(".", "homie/device.py")
-freeze(".", "homie/micro.py")
-freeze(".", "homie/node.py")
-freeze(".", "homie/property.py")
-freeze(".", "homie/utils.py")
+freeze(
+    ".",
+    (
+        "homie/__init__.py",
+        "homie/constants.py",
+        "homie/device.py",
+        "homie/micro.py",
+        "homie/node.py",
+        "homie/property.py",
+        "homie/utils.py",
+    ),
+)
 
 # Libs
-freeze("./lib", ("mqtt_as.py", "asyn.py", "aswitch.py",))
+freeze("./lib", ("mqtt_as.py", "asyn.py", "aswitch.py", "inisetup.py"))
 
 # uasyncio
-freeze("./lib", "uasyncio/__init__.py")
-freeze("./lib", "uasyncio/core.py")
+freeze("./lib", ("uasyncio/__init__.py", "uasyncio/core.py",))
