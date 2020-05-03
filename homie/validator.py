3604a228c86e98f34e619b494e26380c0d4b0657
Rafael Römhild <rafael@roemhild.de>
2020-05-03 22:48:36 +0200

    Validate integer datatype

--- homie/validator.py
+++ homie/validator.py
@@ -17,18 +17,35 @@ def payload_is_valid(cls, payload):
 
     if _dt == STRING:
         pass
-    elif _dt == INTEGER or _dt == FLOAT:
+
+    elif _dt == INTEGER:
+        try:
+            i = int(payload)
+            if _fmt != None:
+                first, last = _fmt.split(":")
+                first = int(first)
+                last = int(last)
+
+                if i < first or i > last:
+                    return False
+        except ValueError:
+            return False
+
+    elif _dt == FLOAT:
         try:
             float(payload)
         except ValueError:
             return False
+
     elif _dt == BOOLEAN:
         if payload != TRUE and payload != FALSE:
             return False
+
     elif _dt == ENUM:
         _values = cls.format.split(",")
         if payload not in _values:
             return False
+
     elif _dt == COLOR:
         if _fmt == RGB:
             if len(payload.split(",")) != 3:
