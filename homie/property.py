4f2195e5094d8e90c3b8cde43c7a4d58a6730d86
Rafael Römhild <rafael@roemhild.de>
2020-10-20 18:50:27 +0200

    Always publish an assigned value even if it not changes
    Do disable this behavior and only publish an update when the value
    changed set HomieProperty(pub_on_upd=True)

--- homie/property.py
+++ homie/property.py
@@ -17,6 +17,7 @@ class BaseProperty:
         default=None,
         restore=True,
         on_message=None,
+        pub_on_upd=True,
     ):
         self._value = default
 
@@ -29,6 +30,7 @@ class BaseProperty:
         self.format = format
         self.restore = restore
         self.on_message = on_message
+        self.pub_on_upd = pub_on_upd
 
         self.topic = None
         self.node = None
@@ -49,9 +51,11 @@ class BaseProperty:
 
     @value.setter
     def value(self, value):
-        """ Set value if changed and publish to mqtt """
+        """Assign new value if changed or self.pub_on_upd is True and publish to mqtt"""
         if value != self._value:
             self._value = value
+            self.publish()
+        elif self.pub_on_upd:
             self.publish()
 
     def set_topic(self):
